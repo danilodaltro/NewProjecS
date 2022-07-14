2d453a02e7c37c4e8a50f690c43c425fd3d79dd2
danilodaltro <danilodaltro@hotmail.com>
2022-07-14 16:37:43 -0300

    feat(master): Criação do ProjetoS

---
+++ src/ProjetoS.Infra.Data/Data/Migrations/20220714152315_Initial.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoS.Infra.Data.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Cidade",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    UF = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cidade", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pessoa",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    Id_Cidade = table.Column<int>(type: "int", nullable: false),
+                    Idade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoa", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pessoa_Cidade_Id_Cidade",
+                        column: x => x.Id_Cidade,
+                        principalSchema: "dbo",
+                        principalTable: "Cidade",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pessoa_Id_Cidade",
+                schema: "dbo",
+                table: "Pessoa",
+                column: "Id_Cidade");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoa",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Cidade",
+                schema: "dbo");
+        }
+    }
+}
---
+++ src/ProjetoS.Infra.Data/Data/Migrations/20220714191035_Second.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoS.Infra.Data.Data.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pessoa_Cidade_Id_Cidade",
+                schema: "dbo",
+                table: "Pessoa");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pessoa_Cidade_Id_Cidade",
+                schema: "dbo",
+                table: "Pessoa",
+                column: "Id_Cidade",
+                principalSchema: "dbo",
+                principalTable: "Cidade",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pessoa_Cidade_Id_Cidade",
+                schema: "dbo",
+                table: "Pessoa");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pessoa_Cidade_Id_Cidade",
+                schema: "dbo",
+                table: "Pessoa",
+                column: "Id_Cidade",
+                principalSchema: "dbo",
+                principalTable: "Cidade",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
